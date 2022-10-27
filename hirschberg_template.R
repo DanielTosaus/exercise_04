b08a2d08045be14676724fe50350e4a660133cd9
Daniel Tosaus <249935@vutbr.cz>
2022-10-27 10:47:05 +0200

    Debuging proccess

--- hirschberg_template.R
+++ hirschberg_template.R
@@ -12,10 +12,10 @@ hirschberg_template <- function(seq1, seq2, align, match, mismatch, gap){
     first_align_row <- align[[1]] # initialize the first row of alignment
     second_align_row <- align[[2]] # initialize the second row of alignment
   
-  
+    
     if(length(seq1)==0) # length of seq1 is equal to zero
     {
-        for(i in 1:length(seq2)) # for each character in seq2
+        for(i in (1:length(seq2))) # for each character in seq2
         {
             first_align_row <- c(first_align_row,'-') # add gap
             second_align_row <- c(second_align_row, seq2[i]) # add character from seq2
@@ -25,7 +25,7 @@ hirschberg_template <- function(seq1, seq2, align, match, mismatch, gap){
     }
     else if(length(seq2)==0) # length of seq2 is equal to zero
     {
-        for(i in 1:length(seq1)) # for each character in seq1
+        for(i in (1:length(seq1))) # for each character in seq1
         {
             first_align_row <- c(first_align_row, seq1[i]) # add character from seq1
             second_align_row <- c(second_align_row,'-') # add gap
@@ -42,15 +42,18 @@ hirschberg_template <- function(seq1, seq2, align, match, mismatch, gap){
     }
     else  #NW algorithm
     {
+        
         x_len <- length(seq1)   # length of seq1
         x_mid <- x_len/2        # half of the length of seq1
         y_len <- length(seq2)   # length of seq2
         
         left_score <- NWScore(seq1[1:x_mid], seq2)# NW score for the first half of seq1 and the whole seq2
-        right_score <-NWScore(reverse(seq1[x_mid:x_len]), reverse(seq2)) # NW score for the second half of seq1 and the whole seq2 (both are reversed)
-        sum <- left_score + reverse(right_score)
+        right_score <-NWScore(reverse(seq1[(x_mid+1):x_len]), reverse(seq2)) # NW score for the second half of seq1 and the whole seq2 (both are reversed)
+        print(right_score)
+        print(right_score)
+        sum <- left_score + rev(right_score)
         y_mid <- which.max(sum)-1  # index of division for seq2
-
+        print(y_mid)
         # The first half
         if(ymid==0) # index of division for seq2 is equal to 0
         {
@@ -64,15 +67,15 @@ hirschberg_template <- function(seq1, seq2, align, match, mismatch, gap){
         # The second half
         if ((x_mid + 1) > x_len) # seq1 cannot be further divided
         {
-            align <- hirschberg_template(DNAString(""), seq2[y_mid, y_len], align, match, mismatch, gap)# call hirschberg function for an empty DNAString object and the second half of seq2
+            align <- hirschberg_template(DNAString(""), seq2[(y_mid+1), y_len], align, match, mismatch, gap)# call hirschberg function for an empty DNAString object and the second half of seq2
         }
         else if ((y_mid + 1) > y_len) # seq2 cannot be further divided
         {
-            align <- hirschberg_template(seq1[x_mid:x_len], DNAString(""), align, match, mismatch, gap)# call hirschberg function for the second half of seq1 and for an empty DNAString object
+            align <- hirschberg_template(seq1[(x_mid+1):x_len], DNAString(""), align, match, mismatch, gap)# call hirschberg function for the second half of seq1 and for an empty DNAString object
         }
         else 
         {
-            Align <- hirschberg_template(seq1[x_mid:x_len], seq2[y_mid, y_len], align, match, mismatch, gap)# call hirschberg function for the second half of seq1 and the second part of seq2
+            align <- hirschberg_template(seq1[(x_mid+1):x_len], seq2[(y_mid+1), y_len], align, match, mismatch, gap)# call hirschberg function for the second half of seq1 and the second part of seq2
         }
     }
     return(align)
@@ -85,13 +88,13 @@ NWScore <- function(X,Y){
   S <- matrix(nrow=length(X)+1, ncol=length(Y)+1)
   S[1,1] <- 0
   
-  for(j in 2:length(Y)){
-    S[1,j] <- gap*(i-1)
+  for(j in (2:length(Y))){
+    S[1,j] <- gap*(j-1)
   }
   
-  for(i in 2:length(X)){
+  for(i in (2:length(X))){
     S[i,1] <- gap*(i-1)
-    for(j in 2:lenght(Y)){
+    for(j in (2:length(Y))){
       if(X[i]==Y[j]){
         scoreSub = S[i-1, j - 1] + match
       }
@@ -101,12 +104,12 @@ NWScore <- function(X,Y){
       
       scoreDel = S[i-1, j] + gap
       scoreIns = S[i, j - 1] + gap
-      Score(1, j) = max(scoreSub, scoreDel, scoreIns)
+      S[1, j] = max(scoreSub, scoreDel, scoreIns)
     }
   }
   LastLine <- c(1:length(Y))
-  for(j in 1:length(Y)){
-    LastLine(j) = Score(1, j)
+  for(j in (1:length(Y))){
+    LastLine[j] = S[1, j]
   }
   LastLine
 }
@@ -117,3 +120,39 @@ mismatch <- -1
 gap <- -2
 
 hirschberg_template(seq1, seq2, c(), match, mismatch, gap)
+
+
+
+
+
+X <- seq2
+Y <- seq1
+S <- matrix(nrow=length(X)+1, ncol=length(Y)+1)
+S[1,1] <- 0
+
+for(j in (2:(length(Y)+1))){
+  S[1,j] <- gap*(j-1)
+}
+for(i in (2:(length(X)+1))){
+  S[i,1] <- gap*(i-1)
+}
+for(i in (2:length(X))){
+  
+  for(j in (2:length(Y))){
+    if(X[i]==Y[j]){
+      scoreSub = S[i-1, j - 1] + match
+    }
+    else{
+      scoreSub = S[i-1, j - 1] + mismatch
+    }
+    
+    scoreDel = S[i-1, j] + gap
+    scoreIns = S[i, j - 1] + gap
+    S[i, j] = max(scoreSub, scoreDel, scoreIns)
+  }
+}
+LastLine <- c(1:length(Y))
+for(j in (1:length(Y))){
+  LastLine[j] = S[1, j]
+}
+  
